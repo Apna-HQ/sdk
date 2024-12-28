d878e98015fc54bb6b73d7914a241bb07b61289d
Yadunandan Batchu <nandubatchu@gmail.com>
2024-12-28 21:59:47 +0000

    setup apnaprovider for react

---
+++ src/client/react/index.ts
@@ -0,0 +1,40 @@
+"use client";
+
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { ApnaApp } from "..";
+
+interface ApnaContextType {
+  nostr: any;
+}
+
+const ApnaContext = createContext<ApnaContextType | null>(null);
+
+export const useApna = () => {
+  const context = useContext(ApnaContext);
+  if (!context) {
+    throw new Error("useNostr must be used within a NostrProvider");
+  }
+  return context;
+};
+
+export function NostrProvider({ children }: { children: React.ReactNode }) {
+  const [apna, setApna] = useState<any>();
+
+  useEffect(() => {
+    const init = async () => {
+      if (!apna) {
+        const apna = new ApnaApp({ appId: "apna-nostr-mvp-1" });
+        setApna(apna.nostr);
+      }
+      console.log(
+        "nostr.getProfile return value: ",
+        await apna.nostr.getProfile()
+      );
+    };
+    init();
+  }, []);
+
+  return (
+    <ApnaContext.Provider value={{ apna }}>{children}</ApnaContext.Provider>
+  );
+}
