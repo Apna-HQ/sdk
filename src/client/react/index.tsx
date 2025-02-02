82e1eda19228d8bf1c4c055ee3c88196e4235565
Yadunandan Batchu <nandubatchu@gmail.com>
2025-02-02 20:40:22 +0000

    add dynamic components

--- src/client/react/index.tsx
+++ src/client/react/index.tsx
@@ -2,6 +2,7 @@
 
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { ApnaApp } from "..";
+import { withDynamicComponent, DynamicComponentProvider, useDynamicComponent } from "./with-dynamic-component";
 
 let apna: ApnaApp;
 
@@ -41,3 +42,5 @@ export function NostrProvider({ children }: { children: React.ReactNode }) {
     <NostrContext.Provider value={{ nostr }}>{children}</NostrContext.Provider>
   );
 }
+
+export { withDynamicComponent, DynamicComponentProvider, useDynamicComponent };
